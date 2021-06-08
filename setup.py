a0960b98f4fde434e12ccab8560bf0d05439777e
Tomas Silva <tomas.silva@acepta.com>
2021-06-08 11:41:46 -0400

    v 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	author='ditat.io',
 	description='Ditat Machine Learning Implementation',
 	name='ditat_ml',
-	version='0.1.0',
+	version='0.1.1',
 	packages=find_packages(include=['ditat_ml', 'ditat_ml.*']),
 	python_requires='>=3.6',
 	install_requires=[
