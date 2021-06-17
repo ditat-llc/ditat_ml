2d7391da2f1e62f7ae101b7cac29bed3668928e7
Tomas Silva <tomas.silva@acepta.com>
2021-06-17 15:45:20 -0400

    new process_df and config loading for persona

--- ditat_ml/persona.py
+++ ditat_ml/persona.py
@@ -14,7 +14,7 @@ from .utility_functions import (
 class Persona:
 	MAPPING = {'Founder': {'seniority': 10, 'function': 'Firm Leader'}, 'CEO': {'seniority': 10, 'function': 'Firm Leader'}, 'Chief Executive Officer': {'seniority': 10, 'function': 'Firm Leader'}, 'Co-Founder': {'seniority': 10, 'function': 'Firm Leader'}, 'Co Founder': {'seniority': 10, 'function': 'Firm Leader'}, 'President': {'seniority': 10, 'function': 'Firm Leader'}, 'Executive Director': {'seniority': 10, 'function': 'Firm Leader'}, 'Chairman': {'seniority': 10, 'function': 'Firm Leader'}, 'Senior Managing Director': {'seniority': 10, 'function': 'Firm Leader'}, 'Senior Partner': {'seniority': 10, 'function': 'Firm Leader'}, 'Managing Principal': {'seniority': 10, 'function': 'Firm Leader'}, 'Managing Partner': {'seniority': 10, 'function': 'Firm Leader'}, 'Managing Director': {'seniority': 10, 'function': 'Firm Leader'}, 'Chief Operating Officer': {'seniority': 9, 'function': 'Operations'}, 'COO': {'seniority': 9, 'function': 'Operations'}, 'Operations': {'seniority': 5, 'function': 'Operations'}, 'Ops': {'seniority': 5, 'function': 'Operations'}, 'Chief Financial Officer': {'seniority': 9, 'function': 'Finance'}, 'CFO': {'seniority': 9, 'function': 'Finance'}, 'Controller': {'seniority': 7, 'function': 'Finance'}, 'Treasurer': {'seniority': 7, 'function': 'Finance'}, 'Fund Controller': {'seniority': 7, 'function': 'Finance'}, 'Vice President-Finance': {'seniority': 5, 'function': 'Finance'}, 'Vice President & Treasurer': {'seniority': 5, 'function': 'Finance'}, 'Director-Finance': {'seniority': 5, 'function': 'Finance'}, 'Director of Finance': {'seniority': 5, 'function': 'Finance'}, 'Finance Director': {'seniority': 5, 'function': 'Finance'}, 'Vice President-Finance & Administration': {'seniority': 5, 'function': 'Finance'}, 'Vice President-Finance & Treasurer': {'seniority': 5, 'function': 'Finance'}, 'VP Finance': {'seniority': 5, 'function': 'Finance'}, 'Senior Vice President-Finance': {'seniority': 5, 'function': 'Finance'}, 'Vice President & Controller': {'seniority': 5, 'function': 'Finance'}, 'Financial Controller': {'seniority': 5, 'function': 'Finance'}, 'Comptroller': {'seniority': 5, 'function': 'Finance'}, 'Vice President of Finance': {'seniority': 5, 'function': 'Finance'}, 'VP of Finance': {'seniority': 5, 'function': 'Finance'}, 'Director-Treasury': {'seniority': 5, 'function': 'Finance'}, 'Director-Finance & Administration': {'seniority': 5, 'function': 'Finance'}, 'Director-Treasury Services': {'seniority': 5, 'function': 'Finance'}, 'Vice President, Finance': {'seniority': 5, 'function': 'Finance'}, 'Vice President-Business & Finance': {'seniority': 5, 'function': 'Finance'}, 'Director-Treasury Operations': {'seniority': 5, 'function': 'Finance'}, 'Financial Director': {'seniority': 5, 'function': 'Finance'}, 'Finance Manager': {'seniority': 5, 'function': 'Finance'}, 'Accounting Manager': {'seniority': 5, 'function': 'Finance'}, 'Treasury Manager': {'seniority': 5, 'function': 'Finance'}, 'Manager-Treasury Operations': {'seniority': 5, 'function': 'Finance'}, 'Financial Manager': {'seniority': 5, 'function': 'Finance'}, 'Director-Accounting': {'seniority': 5, 'function': 'Finance'}, 'Assistant Treasurer': {'seniority': 2, 'function': 'Finance'}, 'Senior Accountant': {'seniority': 2, 'function': 'Finance'}, 'Financial Analyst': {'seniority': 2, 'function': 'Finance'}, 'Fund Accountant': {'seniority': 2, 'function': 'Finance'}, 'Fund Administrator': {'seniority': 2, 'function': 'Finance'}, 'Senior Financial Analyst': {'seniority': 2, 'function': 'Finance'}, 'Accountant': {'seniority': 2, 'function': 'Finance'}, 'Cash Manager': {'seniority': 2, 'function': 'Finance'}, 'Staff Accountant': {'seniority': 2, 'function': 'Finance'}, 'Finance Associate': {'seniority': 3, 'function': 'Finance'}, 'Finance Assistant': {'seniority': 2, 'function': 'Finance'}, 'CPA': {'seniority': 2, 'function': 'Finance'}, 'Associate Treasurer': {'seniority': 3, 'function': 'Finance'}, 'Investment Accountant': {'seniority': 2, 'function': 'Finance'}, 'Partner, Investor Relations': {'seniority': 7, 'function': 'Investor Relations'}, 'Head of Investor Relations': {'seniority': 7, 'function': 'Investor Relations'}, 'Head of IR': {'seniority': 7, 'function': 'Investor Relations'}, 'Managing Director, Investor Relations': {'seniority': 8, 'function': 'Investor Relations'}, 'Managing Director, Client Management': {'seniority': 7, 'function': 'Investor Relations'}, 'Senior Vice President-Investor Relations': {'seniority': 7, 'function': 'Investor Relations'}, 'Director of Marketing': {'seniority': 7, 'function': 'Investor Relations'}, 'Marketing Director': {'seniority': 7, 'function': 'Investor Relations'}, 'Director of Communications': {'seniority': 7, 'function': 'Investor Relations'}, 'Chief Marketing Officer': {'seniority': 8, 'function': 'Investor Relations'}, 'CMO': {'seniority': 8, 'function': 'Investor Relations'}, 'Investor Relations Associate': {'seniority': 4, 'function': 'Investor Relations'}, 'Associate, Investor Relations': {'seniority': 4, 'function': 'Investor Relations'}, 'Investor Relations Analyst': {'seniority': 3, 'function': 'Investor Relations'}, 'Investor Relations Coordinator': {'seniority': 3, 'function': 'Investor Relations'}, 'Marketing Associate': {'seniority': 4, 'function': 'Investor Relations'}, 'Marketing Coordinator': {'seniority': 3, 'function': 'Investor Relations'}, 'Marketing Assistant': {'seniority': 2, 'function': 'Investor Relations'}, 'Marketing': {'seniority': 5, 'function': 'Investor Relations'}, 'Partner': {'seniority': 9, 'function': 'Deal Team'}, 'Vice President': {'seniority': 6, 'function': 'Deal Team'}, 'Senior Vice President': {'seniority': 6, 'function': 'Firm Leader'}, 'Director': {'seniority': 7, 'function': 'Deal Team'}, 'Principal': {'seniority': 7, 'function': 'Deal Team'}, 'Associate': {'seniority': 4, 'function': 'Deal Team'}, 'Co': {'seniority': 8, 'function': 'Firm Leader'}, 'Finance': {'seniority': 3, 'function': 'Finance'}, 'General Partner': {'seniority': 8, 'function': 'Firm Leader'}, 'Investor Relations': {'seniority': 3, 'function': 'Investor Relations'}, 'Operating Partner': {'seniority': 8, 'function': 'Operations'}, 'VP': {'seniority': 6, 'function': 'Deal Team'}, 'Executive Vice President': {'seniority': 7, 'function': 'Firm Leader'}, 'Senior Associate': {'seniority': 5, 'function': 'Deal Team'}, 'Venture Partner': {'seniority': 8, 'function': 'Deal Team'}, 'Human Resources': {'seniority': 3, 'function': 'Deal Team'}, 'Analyst': {'seniority': 3, 'function': 'Deal Team'}, 'Manager': {'seniority': 8, 'function': 'Deal Team'}, 'Owner': {'seniority': 10, 'function': 'Firm Leader'}, 'Founding Partner': {'seniority': 10, 'function': 'Firm Leader'}, 'Investments': {'seniority': 4, 'function': 'Deal Team'}, 'Co,Founder': {'seniority': 10, 'function': 'Firm Leader'}, 'SVP': {'seniority': 8, 'function': 'Deal Team'}, 'Business Development': {'seniority': 4, 'function': 'Deal Team'}, 'Private Equity': {'seniority': 4, 'function': 'Deal Team'}, 'Senior Advisor': {'seniority': 7, 'function': 'Deal Team'}, 'Board Member': {'seniority': 9, 'function': 'Firm Leader'}, 'EA': {'seniority': 3, 'function': 'Operations'}, 'Fundraising': {'seniority': 4, 'function': 'Investor Relations'}, 'Consultant': {'seniority': 4, 'function': 'Deal Team'}, 'Senior Analyst': {'seniority': 4, 'function': 'Deal Team'}, 'Head of Business Development': {'seniority': 8, 'function': 'Deal Team'}, 'Secretary': {'seniority': 3, 'function': 'Operations'}, 'Growth Equity Investor': {'seniority': 5, 'function': 'Deal Team'}, 'Development': {'seniority': 4, 'function': 'Deal Team'}, 'Information Technology': {'seniority': 4, 'function': 'Technology'}, 'Vc': {'seniority': 5, 'function': 'Deal Team'}, 'Investment Professional': {'seniority': 4, 'function': 'Deal Team'}, 'Assistant Vice President': {'seniority': 5, 'function': 'Deal Team'}, 'Human Capital': {'seniority': 4, 'function': 'Deal Team'}, 'Chief Financial': {'seniority': 8, 'function': 'Finance'}, 'EVP': {'seniority': 8, 'function': 'Firm Leader'}, 'Trustee': {'seniority': 7, 'function': 'Deal Team'}, 'Media': {'seniority': 4, 'function': 'Deal Team'}, 'Telecommunications': {'seniority': 3, 'function': 'Technology'}, 'Investment Team': {'seniority': 4, 'function': 'Deal Team'}, 'Senior Principal': {'seniority': 8, 'function': 'Firm Leader'}, 'Client Services': {'seniority': 3, 'function': 'Investor Relations'}, 'Client Service Manager': {'seniority': 5, 'function': 'Investor Relations'}, 'Team Member': {'seniority': 3, 'function': 'Deal Team'}, 'Client Service': {'seniority': 3, 'function': 'Investor Relations'}, 'Senior Managing Partner': {'seniority': 8, 'function': 'Deal Team'}, 'Venture Capital': {'seniority': 7, 'function': 'Deal Team'}, 'Investment Committee': {'seniority': 6, 'function': 'Deal Team'}, 'Business Analyst': {'seniority': 3, 'function': 'Deal Team'}, }
 	
-	PERSONA_SYNONYMS = {
+	PERSONA_MAPPING = {
 		'Finance': ['finance', 'financial', 'equity', 'portfolio', 'treasury', 'accounting', 'market', 'markets', 'tax', 'investor'],
 		'Investor Relations': ['investor relations', 'ir', 'marketing', 'relationship', 'relationships', 'communications', 'communication', 'sales', 'account'],
 		'Operations': ['operations', 'operating', 'administration', 'compliance', 'office', 'administrative'],
@@ -54,12 +54,85 @@ class Persona:
 	REPLACEMENT_MAPPING_WORDS  = {
 		'and': ',',
 	}
-
-	SENIORITY_LIST = list(SENIORITY_MAPPING.keys())
-	PERSONA_LIST = list(PERSONA_SYNONYMS.keys())
 	
-	def __init__(self):
+	def __init__(
+		self,
+		mapping: dict=None,
+		persona_mapping: dict=None,
+		seniority_mapping: dict=None,
+		combine_mappings: bool=False
+		):
+		'''
+		'''
+		self.combine_mappings = combine_mappings
+		self.mapping = mapping
+		self.seniority_mapping = seniority_mapping
+		self.persona_mapping = persona_mapping
+
 		self.verbose = False
+
+	@property
+	def mapping(self):
+		return self._mapping
+
+	@mapping.setter
+	def mapping(self, value):
+		if value is None:
+			self._mapping = type(self).MAPPING.copy()
+		elif not isinstance(value, dict):
+			raise ValueError('mapping must be a dictionary.')
+		elif self.combine_mappings:
+			self._mapping = type(self).MAPPING.copy()
+			for key, value in value.items():
+				self._mapping[key] = value
+		else:
+			self._mapping = value
+
+	@property
+	def seniority_mapping(self):
+		return self._seniority_mapping
+
+	@seniority_mapping.setter
+	def seniority_mapping(self, value):
+		if value is None:
+			self._seniority_mapping = type(self).SENIORITY_MAPPING.copy()
+		elif not isinstance(value, dict):
+			raise ValueError('seniority_mapping must be a dictionary.')
+		elif self.combine_mappings:
+			self._seniority_mapping = type(self).SENIORITY_MAPPING.copy()
+			for key, value in value.items():
+				self._seniority_mapping[key] = value
+		else:
+			self._seniority_mapping = value
+
+		self._seniority_list = list(self._seniority_mapping.keys())
+
+	@property
+	def persona_mapping(self):
+		return self._persona_mapping
+
+	@persona_mapping.setter
+	def persona_mapping(self, value):
+		if value is None:
+			self._persona_mapping = type(self).PERSONA_MAPPING.copy()
+		elif not isinstance(value, dict):
+			raise ValueError('persona_mapping must be a dictionary.')
+		elif self.combine_mappings:
+			self._persona_mapping = type(self).PERSONA_MAPPING.copy()
+			for key, value in value.items():
+				self._persona_mapping[key] = value
+		else:
+			self._persona_mapping = value
+
+		self._persona_list = list(self._persona_mapping.keys())
+
+	@property
+	def seniority_list(self):
+		return self._seniority_list
+
+	@property
+	def persona_list(self):
+		return self._persona_list
 
 	def printif(self, value):
 		if self.verbose:
@@ -104,7 +177,7 @@ class Persona:
 			# Chief {Something} Officer
 			second = value.split('chief')[1].split('office')[0].strip()
 			temp_function = None
-			for persona, synonym in type(self).PERSONA_SYNONYMS.items():
+			for persona, synonym in self.persona_mapping.items():
 				if second in synonym:
 					temp_function = persona
 					break
@@ -118,11 +191,11 @@ class Persona:
 		elif len(value.split()) == 2:
 			temp_function = None
 			first, second = value.split()
-			for persona, synonym in type(self).PERSONA_SYNONYMS.items():
+			for persona, synonym in self.persona_mapping.items():
 				if first in synonym:
 					temp_function = persona
 					break
-			temp_seniority = type(self).SENIORITY_MAPPING.get(second)
+			temp_seniority = self.seniority_mapping.get(second)
 			if temp_function and temp_seniority:
 				return {'seniority': temp_seniority, 'function': temp_function}
 
@@ -131,11 +204,11 @@ class Persona:
 		elif len(value.split()) == 3 and value.split()[1] == 'of':
 			temp_function = None
 			first, second, third = value.split()
-			for persona, synonym in type(self).PERSONA_SYNONYMS.items():
+			for persona, synonym in self.persona_mapping.items():
 				if third in synonym:
 					temp_function = persona
 					break
-			temp_seniority = type(self).SENIORITY_MAPPING.get(first)
+			temp_seniority = self.seniority_mapping.get(first)
 			if temp_function and temp_seniority:
 				return {'seniority': temp_seniority, 'function': temp_function}
 
@@ -145,15 +218,15 @@ class Persona:
 		elif len(value.split()) == 1:
 			temp_function = None
 			temp_seniority = None
-			if value in type(self).SENIORITY_MAPPING:
-				temp_seniority = type(self).SENIORITY_MAPPING[value]
+			if value in self.seniority_mapping:
+				temp_seniority = self.seniority_mapping[value]
 				temp_function = 'Deal Team'
 			else:
-				for persona, synonym in type(self).PERSONA_SYNONYMS.items():
+				for persona, synonym in self.persona_mapping.items():
 					if value in synonym:
 						temp_function = persona
 						break
-				temp_seniority = type(self).SENIORITY_MAPPING['default_']			
+				temp_seniority = self.seniority_mapping['default_']			
 			if temp_function and temp_seniority:
 				return {'seniority': temp_seniority, 'function': temp_function}
 
@@ -162,16 +235,16 @@ class Persona:
 		temp_function = None
 		temp_seniority = None
 
-		for persona, synonym in type(self).PERSONA_SYNONYMS.items():
+		for persona, synonym in self.persona_mapping.items():
 			for v in value.split():
 				if v in synonym:
 					temp_function = persona
 					break
 
 		if temp_function:
-			for s in type(self).SENIORITY_MAPPING:
+			for s in self.seniority_mapping:
 				if s in value.split():
-					temp_seniority = type(self).SENIORITY_MAPPING[s]
+					temp_seniority = self.seniority_mapping[s]
 		
 		if temp_function and temp_seniority:
 			return {'seniority': temp_seniority, 'function': temp_function}
@@ -183,11 +256,11 @@ class Persona:
 			th = 70
 			scorer = fuzz.WRatio
 
-			s_name, s_value = process.extractOne(value, type(self).SENIORITY_LIST, scorer=scorer)
-			p_name, p_value = process.extractOne(value, type(self).PERSONA_LIST, scorer=scorer)
+			s_name, s_value = process.extractOne(value, self.seniority_list, scorer=scorer)
+			p_name, p_value = process.extractOne(value, self.persona_list, scorer=scorer)
 
 			if s_value >= th and p_value >= th:
-				result = {'seniority': type(self).SENIORITY_MAPPING[s_name], 'function': p_name}
+				result = {'seniority': self.seniority_mapping[s_name], 'function': p_name}
 
 		return None
 
@@ -248,7 +321,7 @@ class Persona:
 		if not results:
 			result = None
 
-		elif return_tuples and len(results) > 1:
+		elif return_tuples and len(results) >= 1:
 			result = results
 
 		elif len(results) > 1:
@@ -261,6 +334,16 @@ class Persona:
 		return result
 
 
+	def process_dataframe(self, dataframe, column):
+		'''
+		Convenient method to self.process() a column in a dataframe
+		'''
+		dataframe = dataframe.copy()
+		dataframe['process'] = dataframe[column].apply(lambda x: self.process(x, return_tuples=False))
+		dataframe['seniority'] = dataframe['process'].apply(lambda x: x['seniority'] if x else x)
+		dataframe['function'] = dataframe['process'].apply(lambda x: x['function'] if x else x)
+		dataframe.drop('process', axis=1, inplace=True)
+		return dataframe
 
 
 
